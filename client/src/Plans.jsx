98f87e67a1747e9b02e1b5eb8183e8c7d7ebe095
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2024-01-05 01:45:49 +0530

    connection deployment

--- client/src/Plans.jsx
+++ client/src/Plans.jsx
@@ -75,7 +75,7 @@ const Plans = () => {
         const headers = {
             "Content-Type":"application/json"
         }
-        const response = await fetch("http://localhost:5000/api/create-checkout-session",{
+        const response = await fetch("https://trailerflix-1.onrender.com/api/create-checkout-session",{
             method:"POST",
             headers:headers,
             body:JSON.stringify(body)
--- server/server.js
+++ server/server.js
@@ -32,8 +32,8 @@ app.post("/api/create-checkout-session",async(req,res)=>{
         payment_method_types:["card"],
         line_items:lineItems,
         mode:"payment",
-        success_url:"http://localhost:3000/",
-        cancel_url:"http://localhost:3000/cancel",
+        success_url:"https://trailerflixsam.netlify.app/",
+        cancel_url:"https://trailerflixsam.netlify.app/",
     });
     // app.get('/success', async (req, res) => {
     //     const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
